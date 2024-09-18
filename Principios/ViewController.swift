a8b3b57e7eb993f8654d77bb72146b66505154be
Alejandro Salazar <alejandrosalazarcom25@gmail.com>
2024-09-18 16:09:24 -0600

    first

---
+++ Principios/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Principios
+//
+//  Created by imac on 18/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Hola Mundo")
+        let nombre = "ANAHI"
+        
+    }
+
+
+}
+
